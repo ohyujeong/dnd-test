7636db66434ad2716ad61e85816ac5c7f1488311
ohyujeong <ohlife4@gmail.com>
2022-01-15 21:57:02 +0900

    브랜치 테스트 2

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'DND TEST test22';
+    return 'DND TEST test22 브랜치';
   }
 }
