67650a1d65570cb9c405000d3d3314eb3d7f2482
ohyujeong <ohlife4@gmail.com>
2022-01-25 16:09:05 +0900

    [feat] google 소셜 로그인

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(cookieParser);
   const config = new DocumentBuilder()
     .setTitle('Test API')
     .setDescription('Test Swagger')
