3c5f6c64b28e3228acafdb344fa6e6cf14d3ecae
ohyujeong <ohlife4@gmail.com>
2022-01-25 16:44:32 +0900

    [fix] cookie-parser 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { BlogModule } from './blog/blog.module';
 import { AuthModule } from './auth/auth.module';
 
