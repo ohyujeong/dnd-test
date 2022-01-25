863d015fc158167a374462cffb60806c1944fc45
ohyujeong <ohlife4@gmail.com>
2022-01-25 18:36:42 +0900

    [feat] 구글 로그인 DB 연동

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,12 +3,15 @@ import { Module } from "@nestjs/common";
 import { GoogleStrategy } from "./passport/google.strategy";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './schemas/user.schema';
 
 @Module({
     imports: [
       PassportModule,
+      MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     ],
-    providers: [GoogleStrategy, AuthService],
+    providers: [GoogleStrategy, AuthService, ],
     controllers: [AuthController],
   })
   export class AuthModule {}
